5d24342562a319ebec63fcf3b20b7c11a08e22bf
ErayDev <eraymutabesha13@gmail.com>
2026-09-08 21:34:56 +0200

    SSR ET SSG

---
+++ next/app/mespages/dashboard/page.jsx
@@ -0,0 +1,18 @@
+export default async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/1',{
+        cache:'no-store',//FORCER LE SSR
+    });
+
+    const user =  await res.json();
+
+    return (
+        <div>
+            <h1>bienvenue {user.name}</h1>
+            <p>Derniere connexion :{new Date().toLocaleTimeString()}</p>
+        </div>
+    )
+    
+}
+//SSR :Server-Side Rendering   
+//NPM RUN BUILD
+//SSG: STATIC SITE GENERATION
